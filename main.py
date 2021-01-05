f7334eb92f4dcfc30c8e01f21cb20716b55c4b90
Francisco Lopez <francisco.lopez@wsu.edu>
2021-01-05 12:57:52 -0800

    Fizz Buzz function

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def fizz_it_up():
+    num = input("Let's count to... : ");
+    num = int(num);
+    for i in range(0, num+1):
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 5 == 0:
+            print("Buzz");
+        elif i % 3 == 0:
+            print("Fizz");
+        else:
+            print(i);
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    fizz_it_up();
+
